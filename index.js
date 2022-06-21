81b7705d963e7f5887a6babb9ed299d169a870c4
niola-liberty <niolabakare@gmail.com>
2022-06-21 15:29:12 +0100

    Done, but still need to add extra info for the user.

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-document.getElementById("count-el").innerText = 5
+//PS: all comments are to check for bugs
+//document.getElementById("count-el").innerText = 5
+let countEl = document.getElementById("count-el")
+let saveBtn = document.getElementById("s-btn")
+//console.log(countEl)
 
-let count = 50
-console.log(count)
+
+let count = 0
+
+
+document.getElementById("btn").addEventListener("click", incremnt)
+
+function incremnt() {
+    count = count + 1
+    countEl.innerHTML = count
+    //console.log(count)
+    
+}
+saveBtn.addEventListener("click", save)
+function save(){
+     console.log(count)
+}
+
+
+
+  
